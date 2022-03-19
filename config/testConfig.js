575253992ed9eb2c98ade5456f616a4c63c38465
Darragh Cahill <darragh.cahill@protonmail.com>
2022-03-19 05:02:01 -0700

    updated test accounts

--- config/testConfig.js
+++ config/testConfig.js
@@ -8,15 +8,15 @@ var Config = async function(accounts) {
     // These test addresses are useful when you need to add
     // multiple users in test scripts
     let testAddresses = [
-        "0x69e1CB5cFcA8A311586e3406ed0301C06fb839a2",
-        "0xF014343BDFFbED8660A9d8721deC985126f189F3",
-        "0x0E79EDbD6A727CfeE09A2b1d0A59F7752d5bf7C9",
-        "0x9bC1169Ca09555bf2721A5C9eC6D69c8073bfeB4",
-        "0xa23eAEf02F9E0338EEcDa8Fdd0A73aDD781b2A86",
-        "0x6b85cc8f612d5457d49775439335f83e12b8cfde",
-        "0xcbd22ff1ded1423fbc24a7af2148745878800024",
-        "0xc257274276a4e539741ca11b590b9447b26a8051",
-        "0x2f2899d6d35b1a48a4fbdc93a37a72f264a9fca7"
+        "0xe97cE451C17f76fc73e6A9B348AA83Ad8B268C01",
+        "0xA96FFaF2b4cEfa68A2CCec26734589475E383D8D",
+        "0x96F001D46fA0F1fd85C6A637D70c4224f81712a8",
+        "0xBa183B24c3E526e1BEcd451AFD4eb0197BBbFDaB",
+        "0x3D65e2aD0712F058eb3c11316ea5ddd139eaDe7A",
+        "0xFD1b4A32744aba2f212211d2D1f59E20c5259A2D",
+        "0x4E0A3f25f1Ff35bada1Da143b1a3761564842a6D",
+        "0x6874aCF5A7c4d0Ee85dBB4B4B100f3686e8155B8",
+        "0x8C463ad10D6405A7C57dD1DA07cF308E6AaBfC3b"
     ];
 
 
