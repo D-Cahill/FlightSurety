3be9d9c69aeffbf1125435012e849e2f2fddaae0
Darragh Cahill <darragh.cahill@protonmail.com>
2022-03-20 05:04:31 -0700

    Update to Boiler Plate Code

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,14 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "cactus below soccer pet amused noble goose cry pool noodle tree hawk";
+//cleavar HDWalletProvider = require("truffle-hdwallet-provider");
+//var mnemonic = "cactus below soccer pet amused noble goose cry pool noodle tree hawk";
 
 module.exports = {
   networks: {
     development: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 50);
-      },
+      host: "127.0.0.1",
+      port: 7545,
+      // provider: function() {
+      //   return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 50);
+      // },
       network_id: '*',
       gas: 5999999
     }
